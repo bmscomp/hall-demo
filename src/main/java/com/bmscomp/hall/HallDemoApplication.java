dc82fc0c92d173b2d8929b8c56a33504fe3dd8e8
Said Boudjelda <bmscomp@gmail.com>
2020-07-01 22:22:09 +0200

    initialize spring boot project

---
+++ src/main/java/com/bmscomp/hall/HallDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bmscomp.hall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HallDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HallDemoApplication.class, args);
+	}
+
+}
